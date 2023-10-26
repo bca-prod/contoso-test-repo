c80172aa053ba6dab3d8cf1ff47189873c9b16a4
bca-dev <yeemon4398+dev@gmail.com>
2023-10-26 14:50:53 +0000

    Fix outputs.tf file

--- outputs.tf
+++ outputs.tf
@@ -4,9 +4,9 @@ output "contoso-test-terraform-role-id" {
 
 # Outputs file
 output "meow_app_dns" {
-  value = "http://${aws_eip.hashicat.public_dns}"
+  value = "http://${aws_eip.contoso_eip.public_dns}"
 }
 
 output "meow_app_ip" {
-  value = "http://${aws_eip.hashicat.public_ip}"
+  value = "http://${aws_eip.contoso_eip.public_ip}"
 }
